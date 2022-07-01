3e56249a2118a123e137d510c89b2ba728992e8a
Volozar <svs.elusive@gmail.com>
2022-07-01 12:33:23 +0300

    first commit

---
+++ project02/js/app.js
@@ -0,0 +1,45 @@
+for (var i = 0; i < 10; i++) {
+   
+}
+
+console.log(i);
+
+console.log(num = (21354312315665464123).toString(16))
+console.log(num = (21354312315665464123).toString(26))
+console.log(num = 21354312315665464123..toString(36))
+
+let name = "Ivan";
+console.log(`Привет, ${name}!`);
+
+console.log(`Результат ${1 + 2}`);
+
+console.log(typeof num);
+console.log(typeof(name));
+
+let result = null;
+//result = prompt("Нашел зайца?", ["нет"]);
+console.log(result);
+
+let value1 = '5';
+//value1 = String(value1);
+
+console.log(Number(value1));
+
+value1 %= 2;
+console.log(value1)
+
+
+let apples = "2";
+let oranges = "3";
+
+console.log(apples + oranges);
+console.log(Number(apples) + Number(oranges));
+console.log(+apples + +oranges);
+
+let counter = 1;
+let a = ++counter;
+console.log(a);
+let b = a++;
+console.log(b)
+
+
